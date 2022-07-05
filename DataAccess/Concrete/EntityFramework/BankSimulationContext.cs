9df09b5fd7c9466871020233f42a2e38249db35a
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2022-07-05 23:06:25 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/BankSimulationContext.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class BankSimulationContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BankSimulation;Trusted_Connection=true");
+        }
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BankBranch>().ToTable("BranchesOfBank");
+            modelBuilder.Entity<BankBranch>().Property(p => p.BankBranchId).HasColumnName("Id");
+            modelBuilder.Entity<BankBranch>().Property(p => p.BankBranchName).HasColumnName("Name");
+        }
+
+    }
+}
